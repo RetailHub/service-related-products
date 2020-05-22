d67d3552d03801860a005ed6b8e3055ff42ea556
Sajjan Vaidya <sajjanrajvaidya@gmail.com>
2020-05-22 16:58:26 -0700

    Update cassandra.js to promise based syntax

--- server/database/cassandra/cassandra.js
+++ server/database/cassandra/cassandra.js
@@ -5,29 +5,36 @@ const client = new cassandra.Client({
   localDataCenter: 'datacenter1',
 });
 
-client.connect((err) => {
-  if (err) {
-    console.error('Error connecting to Cassandra', err);
-  } else {
+client.connect()
+  .then(() => {
     console.log('Successfully connected to Cassandra');
-  }
-});
-
-// CREATE KEYSPACE
-client.execute("CREATE KEYSPACE IF NOT EXISTS related WITH REPLICATION = {'class':'SimpleStrategy', 'replication_factor': 1}");
-
-// DROP EXISTING TABLE
-client.execute('DROP TABLE IF EXISTS related.products');
-// CREATE TABLE
-client.execute(`CREATE TABLE IF NOT EXISTS related.products (
-    id uuid,
-    productId INT,
-    name text,
-    price DECIMAL,
-    prime BOOLEAN,
-    imageUrl text,
-    reviews INT,
-    rating float,
-    category text,
-    PRIMARY KEY (productId, name, price, prime, imageUrl, category)
-    )`);
+  })
+  .then(() => {
+  // CREATE KEYSPACE
+    client.execute("CREATE KEYSPACE IF NOT EXISTS related WITH REPLICATION = {'class':'SimpleStrategy', 'replication_factor': 1}");
+  })
+  .then(() => {
+  // DROP EXISTING TABLE
+    client.execute('DROP TABLE IF EXISTS related.products');
+  })
+  .then(() => {
+  // CREATE TABLE
+    client.execute(`CREATE TABLE IF NOT EXISTS related.products (
+      productId INT,
+      name text,
+      price DECIMAL,
+      prime BOOLEAN,
+      imageUrl text,
+      reviews INT,
+      rating float,
+      category text,
+      subcategory text,
+      PRIMARY KEY (productId, name, price, prime, imageUrl, category)
+      )`);
+  })
+  .then(() => {
+    console.log('Created keyspace related with a table products');
+  })
+  .catch((err) => {
+    if (err) console.error('An error occurred');
+  });
