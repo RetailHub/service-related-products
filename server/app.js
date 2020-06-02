d4dd266a86b07a39104c827debad106eb018e0d7
Sajjan Vaidya <sajjanrajvaidya@gmail.com>
2020-06-01 20:29:24 -0700

    Restore index.js and app.js to previous version

--- server/app.js
+++ server/app.js
@@ -6,20 +6,13 @@ const path = require('path');
 
 const routes = require('./routes');
 
-const app1 = express();
-const app2 = express();
+const app = express();
 
-app1
-  .use(express.static(path.join(__dirname, '/../public')))
-  .use(express.json())
-  .use(express.urlencoded({ extended: true }))
-  .use('/api', routes);
+app.use(express.static(path.join(__dirname, '/../public')));
 
-app2
-  .use(express.static(path.join(__dirname, '/../public')))
-  .use(express.json())
-  .use(express.urlencoded({ extended: true }))
-  .use('/api', routes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-module.exports.app1 = app1;
-module.exports.app2 = app2;
+app.use('/api', routes);
+
+module.exports = app;
